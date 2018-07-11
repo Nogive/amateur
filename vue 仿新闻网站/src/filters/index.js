848137de1bfbf75e320ea658513f82a13adf9ce1
chendan <603446367@qq.com>
2018-07-11 17:12:05 +0800

    filter

--- vue 仿新闻网站/src/filters/index.js
+++ vue 仿新闻网站/src/filters/index.js
@@ -1,3 +1,4 @@
+//默认时间
 const normalTime = time => {
   if (time) {
     var oDate = new Date();
@@ -14,10 +15,57 @@ const normalTime = time => {
     return y + "-" + m + "-" + d + " " + h + ":" + m + ":" + s;
   }
 };
+//补零
 const fillZero = num => {
   return num < 10 ? "0" + num : num;
 };
+//带参数
+//货币形式
+const currency = (value, unit, decimal) => {
+  let reg = /^[0-9]+.?[0-9]*$/;
+  if (!reg.test(value)) return "";
+  value = decimal == undefined ? value : value.toFixed(decimal);
+  return `${unit}${value}`;
+};
+//首字母大写
+const capitalize = value => {
+  if (!value) return "";
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
+//时间戳转换为指定格式时间
+const timeFormat = (value, format) => {
+  let date = new Date(value);
+  let y = date.getFullYear();
+  let m = date.getMonth() + 1;
+  let d = date.getDate();
+  let h = date.getHours();
+  let min = date.getMinutes();
+  let s = date.getSeconds();
+  let result = "";
+  if (format == undefined) {
+    result = `${y}-${m < 10 ? "0" + m : m}-${d < 10 ? "0" + d : d} ${
+      h < 10 ? "0" + h : h
+    }:${min < 10 ? "0" + min : min}:${s < 10 ? "0" + s : s}`;
+  }
+  if (format == "yyyy-mm-dd") {
+    result = `${y}-${m < 10 ? "0" + m : m}-${d < 10 ? "0" + d : d}`;
+  }
+  if (format == "yyyy-mm") {
+    result = `${y}-${m < 10 ? "0" + m : m}`;
+  }
+  if (format == "mm-dd") {
+    result = ` ${mm < 10 ? "0" + mm : mm}:${ddmin < 10 ? "0" + dd : dd}`;
+  }
+  if (format == "hh:mm") {
+    result = ` ${h < 10 ? "0" + h : h}:${min < 10 ? "0" + min : min}`;
+  }
+  return result;
+};
 export default {
   normalTime,
-  fillZero
+  fillZero,
+  currency,
+  capitalize,
+  timeFormat
 };
