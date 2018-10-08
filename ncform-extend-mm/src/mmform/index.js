ff72b08d16ace73e6697cc07fe51f752d5ee4ae9
chendan <603446367@qq.com>
2018-10-08 12:50:24 +0800

    fix

--- ncform-extend-mm/src/mmform/index.js
+++ ncform-extend-mm/src/mmform/index.js
@@ -1,15 +1,15 @@
 import Vue from "vue";
 import vueNcform from "@ncform/ncform";
-import mmInput from "@/components/controls/mmInput";
-import mmNumber from "@/components/controls/mmNumber";
-import mmTextarea from "@/components/controls/mmTextarea";
-import mmRadio from "@/components/controls/mmRadio";
-import mmCheckbox from "@/components/controls/mmCheckbox";
-import mmSelect from "@/components/controls/mmSelect";
-import mmDatePicker from "@/components/controls/mmDatePicker";
-import mmTimePicker from "@/components/controls/mmTimePicker";
-import mmPullSelect from "@/components/controls/mmPullSelect";
-import mmMulSelect from "@/components/controls/mmMulSelect";
+import mmInput from "./components/mmInput";
+import mmNumber from "./components/mmNumber";
+import mmTextarea from "./components/mmTextarea";
+import mmRadio from "./components/mmRadio";
+import mmCheckbox from "./components/mmCheckbox";
+import mmSelect from "./components/mmSelect";
+import mmDatePicker from "./components/mmDatePicker";
+import mmTimePicker from "./components/mmTimePicker";
+import mmPullSelect from "./components/mmPullSelect";
+import mmMulSelect from "./components/mmMulSelect";
 Vue.use(vueNcform, {
   extComponents: {
     mmInput,
